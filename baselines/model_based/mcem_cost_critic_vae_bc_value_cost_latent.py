67723afd6e3f300287716dd82ebcb4899f7a558a
returaj <returaj.burnwal@gmail.com>
2024-08-17 16:43:18 +0530

    use default values

--- baselines/model_based/mcem_cost_critic_vae_bc_value_cost_latent.py
+++ baselines/model_based/mcem_cost_critic_vae_bc_value_cost_latent.py
@@ -46,9 +46,9 @@ default_cfg = {
     "value_coef": 0.1,  # TDMPC update coef
     "use_dynamics_norm": False,
     "use_critic_norm": False,
-    "elite_portion": 0.1,  # 0.01
-    "num_samples": 40,  # 400
-    "inference_horizon": 2,  # 5
+    "elite_portion": 0.01,  # 0.01
+    "num_samples": 400,  # 400
+    "inference_horizon": 5,  # 5
     "train_horizon": 5,  # 5
 }
 
@@ -457,7 +457,7 @@ def main(args, cfg_env=None):
 
         eval_start_time = time.time()
         is_last_epoch = epoch >= num_epochs - 1
-        eval_episodes = 1 if is_last_epoch else 1
+        eval_episodes = 5 if is_last_epoch else 1
         if args.use_eval:
             for id in range(eval_episodes):
                 eval_done = False
