7d8760f1962949df0ccefc5023222a7f7b7159fc
returaj <returaj123burnwal@gmail.com>
2025-10-05 02:24:24 +0530

    add buffer and model code in jax

---
+++ dsrl_model/utils/buffer_jax.py
@@ -0,0 +1,164 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+
+class OnPolicyBuffer:
+    def __init__(
+        self,
+        rngs,
+        obs_dim,
+        act_dim,
+        neg_data_size,
+        union_data_size,
+        horizon,
+        batch_size,
+        ep_len=1000,
+        priorities_alpha=1.0,
+    ):
+        self.rngs = rngs
+        self.horizon = horizon
+        self.batch_size = batch_size
+        self.neg_capacity = neg_data_size
+        self.union_capacity = union_data_size
+        self.ep_len = ep_len
+        self._priorities_alpha = priorities_alpha
+
+        self.dtype = jnp.float32
+        self._neg_obs = jnp.empty((self.neg_capacity + 1, obs_dim), dtype=self.dtype)
+        self._neg_act = jnp.empty((self.neg_capacity, act_dim), dtype=self.dtype)
+        self._neg_cost = jnp.empty((self.neg_capacity,), dtype=self.dtype)
+        self._neg_priorities = jnp.ones((self.neg_capacity,), dtype=self.dtype)
+
+        self._union_obs = jnp.empty(
+            (self.union_capacity + 1, obs_dim), dtype=self.dtype
+        )
+        self._union_act = jnp.empty((self.union_capacity, act_dim), dtype=self.dtype)
+        self._union_cost = jnp.empty((self.union_capacity,), dtype=self.dtype)
+        self._union_priorities = jnp.ones((self.union_capacity,), dtype=self.dtype)
+        self._eps = 1e-6
+        self._neg_idx = 0
+        self._union_idx = 0
+
+    def _update(
+        self,
+        obs_store,
+        act_store,
+        priority_store,
+        cost_store,
+        idx,
+        obs,
+        act,
+        priority,
+        cost,
+        capacity,
+    ):
+        obs_store[idx : idx + self.ep_len] = jnp.array(obs)
+        act_store[idx : idx + self.ep_len] = jnp.array(act)
+        priority_store[idx : idx + self.ep_len] = jnp.array(priority)
+        cost_store[idx : idx + self.ep_len] = jnp.array(cost)
+        return (idx + self.ep_len) % capacity
+
+    def add(self, obs, act, done, cost=None, is_negative=False):
+        assert cost is not None, "cost field cannot be none"
+        max_priority = 1.0
+        done_sum = np.sum(done) or 1.0
+        true_ep_len = self.ep_len - done_sum + 1
+        mask = jnp.arange(self.ep_len) >= true_ep_len - self.horizon
+        new_priorities = jnp.full((self.ep_len,), max_priority)
+        new_priorities[mask] = 0.0
+
+        if is_negative:
+            self._neg_idx = self._update(
+                obs_store=self._neg_obs,
+                act_store=self._neg_act,
+                priority_store=self._neg_priorities,
+                cost_store=self._neg_cost,
+                idx=self._neg_idx,
+                obs=obs,
+                act=act,
+                priority=new_priorities,
+                cost=cost,
+                capacity=self.neg_capacity,
+            )
+        else:
+            self._union_idx = self._update(
+                obs_store=self._union_obs,
+                act_store=self._union_act,
+                priority_store=self._union_priorities,
+                cost_store=self._union_cost,
+                idx=self._union_idx,
+                obs=obs,
+                act=act,
+                priority=new_priorities,
+                cost=cost,
+                capacity=self.union_capacity,
+            )
+
+    def update_priorities(self, idxs, priorities, union=True):
+        p = self._union_priorities if union else self._neg_priorities
+        p[idxs] = jnp.array(priorities) + self._eps
+
+    def sample(self):
+        batch_size = self.batch_size
+        steps_per_epoch = self.union_capacity // batch_size
+
+        union_probs = self._union_priorities**self._priorities_alpha
+        union_probs /= union_probs.sum()
+        union_total = len(union_probs)
+        union_idxs = jax.random.choice(
+            key=self.rngs(),
+            a=union_total,
+            shape=(steps_per_epoch, batch_size),
+            p=union_probs,
+            replace=True,
+        )
+
+        neg_probs = self._neg_priorities
+        neg_probs /= neg_probs.sum()
+        neg_total = len(neg_probs)
+        neg_idxs = jax.random.choice(
+            key=self.rngs(),
+            a=neg_total,
+            shape=(steps_per_epoch, batch_size),
+            p=neg_probs,
+            replace=True,
+        )
+
+        @jax.jit
+        def sample_step(nidx, uidx):
+            h_neg_obs = jnp.empty(
+                (self.horizon, batch_size, *self._neg_obs.shape[1:]), dtype=self.dtype
+            )
+            h_neg_act = jnp.empty(
+                (self.horizon, batch_size, *self._neg_act.shape[1:]), dtype=self.dtype
+            )
+            h_neg_cost = jnp.empty((self.horizon, batch_size), dtype=self.dtype)
+            h_union_obs = jnp.empty_like(h_neg_obs)
+            h_union_act = jnp.empty_like(h_neg_act)
+            h_union_cost = jnp.empty_like(h_neg_cost)
+
+            def body_fun(t):
+                _nidx, _uidx = nidx + t, uidx + t
+                h_neg_obs[t] = self._neg_obs[_nidx]
+                h_neg_act[t] = self._neg_act[_nidx]
+                h_neg_cost[t] = self._neg_cost[_nidx]
+                h_union_obs[t] = self._union_obs[_uidx]
+                h_union_act[t] = self._union_act[_uidx]
+                h_union_cost[t] = self._union_cost[_uidx]
+                return t + 1
+
+            _ = jax.lax.while_loop(lambda t: t < self.horizon, body_fun, 0)
+
+            return (
+                h_neg_obs,
+                h_neg_act,
+                h_neg_cost,
+                h_union_obs,
+                h_union_act,
+                h_union_cost,
+                uidx,
+            )
+
+        for nidx, uidx in zip(neg_idxs, union_idxs):
+            yield sample_step(nidx, uidx)
---
+++ dsrl_model/utils/models_jax.py
@@ -0,0 +1,212 @@
+import distrax
+import jax
+import jax.numpy as jnp
+from flax import nnx
+
+EPS = 1e-7
+
+
+def gumbel_softmax(key, logits, tau, hard=False):
+    g = distrax.Gumbel(loc=0.0, scale=1.0).sample(seed=key, sample_shape=logits.shape)
+    ysoft = jax.nn.softmax((g + logits) / tau, axis=-1)
+
+    if not hard:
+        return ysoft
+
+    yhard = jax.nn.one_hot(jnp.argmax(ysoft, axis=-1), ysoft.shape[1])
+    yhard = yhard + ysoft - jax.lax.stop_gradient(ysoft)
+    return yhard
+
+
+def l2_normalize(x, axis=None, eps=EPS):
+    return x * jax.lax.rsqrt((x * x).sum(axis=axis, keepdims=True) + eps)
+
+
+class SafeDiceTanhMixtureActor(nnx.Module):
+    def __init__(
+        self,
+        rngs,
+        obs_dim,
+        act_dim,
+        hidden_size=256,
+        num_components=2,
+        mean_range=(-7.0, 7.0),
+        logstd_range=(-5.0, 2.0),
+        eps=EPS,
+        mdn_temperature=1.0,
+    ):
+        self.rngs = rngs
+
+        self.act_dim = act_dim
+        self.num_components = num_components
+        self.mdn_temp = mdn_temperature
+
+        self.mean_min, self.mean_max = mean_range
+        self.logstd_min, self.logstd_max = logstd_range
+        self.eps = eps
+
+        self.pre_encoder = nnx.Sequential(
+            nnx.Linear(obs_dim, hidden_size, rngs=rngs),
+            nnx.relu,
+            nnx.Linear(hidden_size, hidden_size, rngs=rngs),
+            nnx.relu,
+        )
+
+        self.logits = nnx.Linear(hidden_size, num_components, rngs=rngs)
+        self.means = nnx.Linear(hidden_size, num_components * act_dim, rngs=rngs)
+        self.logstds = nnx.Linear(hidden_size, num_components * act_dim, rngs=rngs)
+
+    def __call__(self, obs):
+        x = self.pre_encoder(obs)
+
+        mixture_logits = self.logits(x) / self.mdn_temp
+        means = jnp.clip(self.means(x), self.mean_min, self.mean_max)
+        means = means.reshape(-1, self.num_components, self.act_dim)
+        logstds = jnp.clip(self.logstds(x), self.logstd_min, self.logstd_max)
+        logstds = logstds.reshape(-1, self.num_components, self.act_dim)
+        stds = jnp.exp(logstds)
+
+        mixture_dist = distrax.Categorical(logits=mixture_logits)
+        component_dist = distrax.Normal(loc=means, scale=stds)
+        component_dist = distrax.Independent(component_dist, 1)
+        pretanh_action_dist = distrax.MixtureSameFamily(mixture_dist, component_dist)
+
+        mixture_sample = gumbel_softmax(self.rngs(), mixture_logits, tau=1.0, hard=True)
+        component_sample = component_dist.sample(seed=self.rngs())
+        pretanh_actions = jnp.einsum("ij,ijk->ik", mixture_sample, component_sample)
+        actions = jax.nn.tanh(pretanh_actions)
+
+        return actions, pretanh_actions, pretanh_action_dist
+
+    def action(self, obs, deterministic=False):
+        x = self.pre_encoder(obs)
+
+        mixture_logits = self.logits(x) / self.mdn_temp
+        means = jnp.clip(self.means(x), self.mean_min, self.mean_max)
+        means = means.reshape(-1, self.num_components, self.act_dim)
+        logstds = jnp.clip(self.logstds(x), self.logstd_min, self.logstd_max)
+        logstds = logstds.reshape(-1, self.num_components, self.act_dim)
+        stds = jnp.exp(logstds)
+
+        mixture_dist = distrax.Categorical(logits=mixture_logits)
+
+        if deterministic:
+            mixture_id = mixture_dist.sample(seed=self.rngs())
+            pretanh_action = jax.vmap(lambda x, y: x[y])(means, mixture_id)
+        else:
+            component_dist = distrax.Normal(loc=means, scale=stds)
+            component_dist = distrax.Independent(component_dist, 1)
+            pretanh_action_dist = distrax.MixtureSameFamily(
+                mixture_dist, component_dist
+            )
+            pretanh_action = pretanh_action_dist.sample(seed=self.rngs())
+
+        return jax.nn.tanh(pretanh_action)
+
+
+class ExpCostModel(nnx.Module):
+    def __init__(self, rngs, x_dims, hidden_size=256):
+        sizes = [x_dims, hidden_size, hidden_size, 1]
+        layers = list()
+        for j in range(len(sizes) - 1):
+            act = nnx.elu if j < len(sizes) - 2 else jax.nn.identity
+            affine_layer = nnx.Linear(sizes[j], sizes[j + 1], rngs=rngs)
+            layers += [affine_layer, act]
+        self.model = nnx.Sequential(*layers)
+
+    def __call__(self, x):
+        x = jnp.squeeze(self.model(x), axis=-1)
+        return jax.nn.sigmoid(x)
+
+
+def positionalencoding1d(d_model, length):
+    """
+    Code: https://github.com/wzlxjtu/PositionalEncoding2D/blob/master/positionalembedding2d.py
+    """
+    """
+    :param d_model: dimension of the model
+    :param length: length of positions
+    :return: length*d_model position matrix
+    """
+    if d_model % 2 != 0:
+        raise ValueError(
+            "Cannot use sin/cos positional encoding with "
+            "odd dim (got dim={:d})".format(d_model)
+        )
+    pe = jnp.zeros(length, d_model)
+    position = jnp.arange(0, length).unsqueeze(1)
+    div_term = jnp.exp((jnp.arange(0, d_model, 2) * -(jnp.log(10000.0) / d_model)))
+    pe[:, 0::2] = jnp.sin(position * div_term)
+    pe[:, 1::2] = jnp.cos(position * div_term)
+    pe = jnp.expand_dims(pe, axis=0)
+    return pe
+
+
+class TransformerBlock(nnx.Module):
+    def __init__(self, rngs, d_model, liner_features, num_heads, rate=0.1):
+        self.attn = nnx.MultiHeadAttention(num_heads, d_model, rngs=rngs)
+        self.dp1 = nnx.Dropout(rate=rate, rngs=rngs)
+        self.ln1 = nnx.LayerNorm(d_model, rngs=rngs)
+        self.ff = nnx.Sequential(
+            nnx.Linear(d_model, liner_features, rngs=rngs),
+            nnx.relu,
+            nnx.Linear(liner_features, d_model, rngs=rngs),
+        )
+        self.dp2 = nnx.Dropout(rate=rate, rngs=rngs)
+        self.ln2 = nnx.LayerNorm(d_model, rngs=rngs)
+
+    def __call__(self, x, mask=None, training=False):
+        # x shape: batch x horizon x d_model
+        attn_x = self.dp1(self.attn(x, mask=mask), deterministic=not training)
+        # residual connection
+        x = x + attn_x
+        x = self.ln1(x)
+
+        ff_x = self.dp2(self.ff(x), deterministic=not training)
+        # residual connection
+        x = x + ff_x
+        x = self.ln2(x)
+        return x
+
+
+class TransformerEmbedding(nnx.Module):
+    def __init__(
+        self,
+        rngs,
+        obs_dim,
+        act_dim,
+        horizon,
+        emb_dim=128,
+        num_heads=4,
+        num_attentions=1,
+    ):
+        self.encoder = nnx.Linear(obs_dim + act_dim, emb_dim, rngs=rngs)
+        self.pos_encoding = positionalencoding1d(emb_dim, horizon)
+        self.mask = jnp.tril(jnp.ones(horizon, horizon))  # causal mask
+        linear_features = 4 * emb_dim
+        self.transformer_blocks = [
+            TransformerBlock(
+                rngs=rngs,
+                d_model=emb_dim,
+                liner_features=linear_features,
+                num_heads=num_heads,
+            )
+            for _ in range(num_attentions)
+        ]
+
+    def __call__(self, x, horizon_axis=1, normalize_z=True, training=False):
+        if horizon_axis != 1:
+            # shape of given x: horizon X batch X obs_act_dim
+            # after permuting x: batch X horizon X obs_act_dim
+            x = x.permute(1, 0, 2)
+
+        x = self.encoder(x)
+        x += self.pos_encoding
+        for transformer in self.transformer_blocks:
+            x = transformer(x, self.mask, training)
+
+        x = x[:, -1, :]
+        if normalize_z:
+            x = l2_normalize(x, axis=-1)
+
+        return x
