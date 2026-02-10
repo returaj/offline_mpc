88df1e00e1f5ac7e8230e32311855cfa36850749
returaj <returaj.burnwal@gmail.com>
2026-02-10 10:58:34 +0530

    rename safemil2 full gpu utilization as default safemil_jax version

--- dsrl_model/model_free/old_safemil_jax.py
+++ dsrl_model/model_free/old_safemil_jax.py
@@ -30,9 +30,7 @@ from dsrl_model.utils.models_jax import (
     get_tree_norm,
 )
 from dsrl_model.utils.native_logger import EpochLogger
-from dsrl_model.utils.utils import make_static_config_from_dict, single_agent_args
-
-# jax.config.update("jax_disable_jit", True)
+from dsrl_model.utils.utils import single_agent_args
 
 EPS = 1e-6
 
@@ -46,11 +44,12 @@ default_cfg = {
     "gamma": 0.99,
     "action_repeat": 1,  # set to 2, min value is 1
     "update_freq": 1,
-    "temp": 0.6,
+    "cost_weight_temp": 0.6,
     "train_horizon": 20,  # 20
     "weight_decay": 0.01,
-    "bc_weight_binary": None,
+    "grad_reg_coeffs": 10.0,
     "total_iteration": int(1e6),
+    "bc_weight_binary": None,
 }
 
 trajectory_cfg = {
@@ -222,213 +221,6 @@ def train_policy_model(
     return loss, *aux_values
 
 
-def policy_loss_grads_fun(
-    cost_model,
-    policy_model,
-    data,
-    config,
-    has_positive,
-):
-    gamma = config.gamma
-    temp = config.temp
-
-    # Batch_Bag X Horizon X obs
-    batch_bag, horizon, _ = data.union_obs.shape
-
-    # Batch_Bag_Horizon X obs/act_dim
-    target_pos_obs = data.pos_obs.reshape(batch_bag * horizon, -1)
-    target_pos_act = data.pos_act.reshape(batch_bag * horizon, -1)
-
-    target_union_obs = data.union_obs.reshape(batch_bag * horizon, -1)
-    target_union_act = data.union_act.reshape(batch_bag * horizon, -1)
-
-    # Batch_Bag_Horizon
-    union_cost = cost_model(jnp.concat([target_union_obs, target_union_act], axis=-1))
-    # Batch_Bag X Horizon
-    horizon_union_cost = union_cost.reshape((batch_bag, horizon))
-    # Batch_Bag
-    batch_bag_union_cost = discounted_sum(horizon_union_cost.T, gamma)
-    # weight = exp(-C / temp) / Mean [ exp(-C / temp) ]
-    exp_union_weight = jnp.exp(-batch_bag_union_cost / temp)
-    union_weight = exp_union_weight / exp_union_weight.mean()
-
-    def loss_fun(policy_model):
-        pred_pos_act, *_ = policy_model(target_pos_obs)
-        flat_pos_loss = optax.l2_loss(pred_pos_act, target_pos_act).sum(axis=-1)
-        horizon_pos_loss = has_positive * flat_pos_loss.reshape((batch_bag, horizon))
-        batch_bag_pos_loss = discounted_sum(horizon_pos_loss.T, gamma)
-        pos_loss = jnp.mean(batch_bag_pos_loss)
-
-        pred_union_act, *_ = policy_model(target_union_obs)
-        flat_union_loss = optax.l2_loss(pred_union_act, target_union_act).sum(axis=-1)
-        horizon_union_loss = flat_union_loss.reshape((batch_bag, horizon))
-        batch_bag_union_loss = discounted_sum(horizon_union_loss.T, gamma)
-        union_loss = jnp.mean(union_weight * batch_bag_union_loss)
-
-        loss = pos_loss + union_loss
-        return loss, (pos_loss, union_loss)
-
-    grad_fun = nnx.value_and_grad(loss_fun, has_aux=True)
-    (loss, aux_values), grads = grad_fun(policy_model)
-    return loss, grads, *aux_values
-
-
-def cost_loss_grads_fun(cost_model, data, config, has_positive):
-    gamma = config.gamma
-    batch_size, bag_size = config.batch_size, config.bag_size
-
-    # Batch_Bag X Horizon X obs_act_dim
-    target_pos = jnp.concat([data.pos_obs, data.pos_act], axis=-1)
-    target_neg = jnp.concat([data.neg_obs, data.neg_act], axis=-1)
-    target_union = jnp.concat([data.union_obs, data.union_act], axis=-1)
-
-    def loss_fun(cost_model):
-        # Batch_Bag x Horizon
-        batch_bag_pos_cost = cost_model(target_pos)
-        batch_bag_neg_cost = cost_model(target_neg)
-        batch_bag_union_cost = cost_model(target_union)
-
-        # Batch_Bag
-        pos_traj_cost = discounted_sum(batch_bag_pos_cost.T, gamma)
-        neg_traj_cost = discounted_sum(batch_bag_neg_cost.T, gamma)
-        union_traj_cost = discounted_sum(batch_bag_union_cost.T, gamma)
-
-        # Batch
-        bag_pos_cost = pos_traj_cost.reshape((batch_size, bag_size)).mean(axis=1)
-        bag_neg_cost = neg_traj_cost.reshape((batch_size, bag_size)).mean(axis=1)
-        bag_union_cost = union_traj_cost.reshape((batch_size, bag_size)).mean(axis=1)
-
-        # min L = - log[ exp(neg) / (exp(neg) + exp(pos)) ] = log[ 1 + exp(pos - neg) ]
-        # L = nn.softplus(pos-neg)
-        pos_neg_loss = has_positive * jax.nn.softplus(bag_pos_cost - bag_neg_cost)
-        pos_union_loss = has_positive * jax.nn.softplus(bag_pos_cost - bag_union_cost)
-        union_neg_loss = jax.nn.softplus(bag_union_cost - bag_neg_cost)
-
-        loss = (pos_neg_loss + pos_union_loss + union_neg_loss).mean()
-
-        return loss, (
-            pos_neg_loss.mean(),
-            pos_union_loss.mean(),
-            union_neg_loss.mean(),
-            has_positive * bag_pos_cost.mean(),
-            bag_neg_cost.mean(),
-            bag_union_cost.mean(),
-        )
-
-    grad_fun = nnx.value_and_grad(loss_fun, has_aux=True)
-    (loss, aux_values), grads = grad_fun(cost_model)
-    return loss, grads, *aux_values
-
-
-def train_step(
-    cost_model,
-    cost_optimizer,
-    policy_model,
-    policy_optimizer,
-    batch_data,
-    config,
-    has_positive,
-):
-    cost_loss, cost_grads, *cost_aux = cost_loss_grads_fun(
-        cost_model=cost_model,
-        data=batch_data,
-        config=config,
-        has_positive=has_positive,
-    )
-    cost_optimizer.update(cost_grads)
-
-    policy_loss, policy_grads, *policy_aux = policy_loss_grads_fun(
-        cost_model=cost_model,
-        policy_model=policy_model,
-        data=batch_data,
-        config=config,
-        has_positive=has_positive,
-    )
-    policy_optimizer.update(policy_grads)
-
-    mean_pos_reward = batch_data.pos_reward.sum(-1).mean()
-    mean_neg_reward = batch_data.neg_reward.sum(-1).mean()
-    mean_union_reward = batch_data.union_reward.sum(-1).mean()
-
-    mean_pos_cost = batch_data.pos_cost.sum(-1).mean()
-    mean_neg_cost = batch_data.neg_cost.sum(-1).mean()
-    mean_union_cost = batch_data.union_cost.sum(-1).mean()
-
-    return (
-        cost_loss,
-        *cost_aux,
-        policy_loss,
-        *policy_aux,
-        mean_pos_reward,
-        mean_neg_reward,
-        mean_union_reward,
-        mean_pos_cost,
-        mean_neg_cost,
-        mean_union_cost,
-    )
-
-
-@nnx.jit
-def train_n_steps(
-    cost_model,
-    cost_optimizer,
-    policy_model,
-    policy_optimizer,
-    data_buffer,
-    config,
-    has_positive,
-    key,
-):
-    num_steps = config.log_freq
-
-    pos_idxs, neg_idxs, union_idxs = data_buffer.sample_idxs(
-        data_buffer, key, num_steps
-    )
-
-    def body_fun(i, carry):
-        (
-            _,
-            cost_model,
-            cost_optimizer,
-            policy_model,
-            policy_optimizer,
-        ) = carry
-
-        batch_data = data_buffer.sample_batch(
-            data_buffer, pos_idxs[i], neg_idxs[i], union_idxs[i]
-        )
-
-        val = train_step(
-            cost_model=cost_model,
-            cost_optimizer=cost_optimizer,
-            policy_model=policy_model,
-            policy_optimizer=policy_optimizer,
-            batch_data=batch_data,
-            config=config,
-            has_positive=has_positive,
-        )
-
-        return (
-            val,
-            cost_model,
-            cost_optimizer,
-            policy_model,
-            policy_optimizer,
-        )
-
-    init_val = (jnp.zeros((), dtype=jnp.float32),) * 16
-    init_carry = (
-        init_val,
-        cost_model,
-        cost_optimizer,
-        policy_model,
-        policy_optimizer,
-    )
-    val, *_ = nnx.fori_loop(0, num_steps, body_fun, init_carry)
-
-    return val, num_steps
-
-
 def main(args, cfg_env=None):
     # set the random seed, device and number of threads
     random.seed(args.seed)
@@ -454,33 +246,30 @@ def main(args, cfg_env=None):
     config = {**default_cfg, **trajectory_cfg}
     config["train_horizon"] = args.train_horizon or config.get("train_horizon")
     config["bag_size"] = args.bag_size or config["bag_size"]
-    config["temp"] = args.cost_weight_temp or config["temp"]
+    config["cost_weight_temp"] = args.cost_weight_temp or config["cost_weight_temp"]
     config["bc_weight_binary"] = args.bc_weight_binary
     config["policy_type"] = args.policy_type
     config["normalize_observation"] = args.normalize_observation
-    config["lr"] = args.lr
-
-    # set training steps
-    batch_size = args.batch_size or config.get("batch_size")
-    config["batch_size"] = batch_size
-
-    config_data = make_static_config_from_dict(name="State", d=config)()
 
     # evaluation environment
     eval_env = gym.make(args.task)
     eval_env.set_target_cost(config["target_cost"])
     eval_env.reset(seed=args.seed)
 
+    # set training steps
+    batch_size = args.batch_size or config.get("batch_size")
+
     # set model
     obs_space, act_space = eval_env.observation_space, eval_env.action_space
-    policy_model = SafeDiceTanhMixtureActor(
+    config["lr"] = args.lr
+    bc_policy = SafeDiceTanhMixtureActor(
         rngs=rngs,
         obs_dim=obs_space.shape[0],
         act_dim=act_space.shape[0],
         hidden_size=config["hidden_size"],
     )
-    policy_optimizer = nnx.Optimizer(
-        model=policy_model,
+    bc_optimizer = nnx.Optimizer(
+        model=bc_policy,
         tx=optax.chain(
             optax.clip_by_global_norm(config["max_grad_norm"]),
             optax.adamw(
@@ -573,7 +362,6 @@ def main(args, cfg_env=None):
         buffer.add(obs, act, done, reward, cost, is_union=True)
 
     buffer.to_jax_ndarray()
-    data_buffer = buffer.get_data_buffer()
 
     # set logger
     eval_rew_deque = deque(maxlen=config["eval_episode_freq"])
@@ -584,114 +372,154 @@ def main(args, cfg_env=None):
 
     logger = EpochLogger(log_dir=args.log_dir, seed=str(args.seed))
     logger.save_config(dict_args)
-    logger.log("Start cost and policy model training.")
+    logger.log("Start cost and bc_policy model training.")
 
     steps = 0
     while steps < config["total_iteration"]:
+        # shape: Batch X Horizon X obs/act_dim
+        for (
+            target_pos_obs,
+            target_pos_act,
+            target_pos_reward,
+            target_pos_cost,
+            target_neg_obs,
+            target_neg_act,
+            target_neg_reward,
+            target_neg_cost,
+            target_union_obs,
+            target_union_act,
+            target_union_reward,
+            target_union_cost,
+            _,
+        ) in buffer.sample():
 
-        val, num_itr = train_n_steps(
-            cost_model=cost_model,
-            cost_optimizer=cost_optimizer,
-            policy_model=policy_model,
-            policy_optimizer=policy_optimizer,
-            data_buffer=data_buffer,
-            config=config_data,
-            has_positive=has_positive,
-            key=rngs.random_sample(),
-        )
+            steps += 1
 
-        (
-            cost_loss,
-            cost_pos_neg_loss,
-            cost_pos_union_loss,
-            cost_union_neg_loss,
-            mean_cost_pos_bag,
-            mean_cost_neg_bag,
-            mean_cost_union_bag,
-            policy_loss,
-            policy_pos_loss,
-            policy_union_loss,
-            mean_pos_reward,
-            mean_neg_reward,
-            mean_union_reward,
-            mean_pos_cost,
-            mean_neg_cost,
-            mean_union_cost,
-        ) = val
-
-        steps += num_itr
-
-        logger.logged = False
-
-        if (steps % config["log_freq"] == 0) and (not logger.logged):
-            eval_episodes = config["eval_episode_freq"]
-            if args.use_eval:
-                eval_start_time = time.time()
-                for id in range(eval_episodes):
-                    (eval_reward, eval_cost, eval_len) = evaluate_bc_policy(
-                        eval_env, policy_model.action, mu_obs, std_obs
-                    )
-                    eval_rew_deque.append(eval_reward)
-                    eval_cost_deque.append(eval_cost)
-                    eval_len_deque.append(eval_len)
-                eval_end_time = time.time()
-
-                logger.log_tabular("Metrics/EvalEpRet", np.mean(eval_rew_deque))
-                logger.log_tabular("Metrics/EvalEpCost", np.mean(eval_cost_deque))
-                logger.log_tabular("Metrics/EvalEpLen", np.mean(eval_len_deque))
-                logger.log_tabular("Time/Eval", eval_end_time - eval_start_time)
-
-            logger.log_tabular("Train/Steps", steps)
-
-            logger.log_tabular("Loss/Loss_cost", cost_loss.item())
-            logger.log_tabular("Loss/Loss_cost_pos_neg", cost_pos_neg_loss.item())
-            logger.log_tabular("Loss/Loss_cost_pos_union", cost_pos_union_loss.item())
-            logger.log_tabular("Loss/Loss_cost_union_neg", cost_union_neg_loss.item())
-
-            logger.log_tabular("Loss/Loss_policy", policy_loss.item())
-            logger.log_tabular("Loss/Loss_policy_pos", policy_pos_loss.item())
-            logger.log_tabular("Loss/Loss_policy_union", policy_union_loss.item())
-
-            logger.log_tabular("CostPred/pos_bag_cost", mean_cost_pos_bag.item())
-            logger.log_tabular("CostPred/neg_bag_cost", mean_cost_neg_bag.item())
-            logger.log_tabular("CostPred/union_bag_cost", mean_cost_union_bag.item())
-
-            logger.log_tabular("Reward/pos", mean_pos_reward.item())
-            logger.log_tabular("Reward/neg", mean_neg_reward.item())
-            logger.log_tabular("Reward/union", mean_union_reward.item())
-
-            logger.log_tabular("Cost/pos", mean_pos_cost.item())
-            logger.log_tabular("Cost/neg", mean_neg_cost.item())
-            logger.log_tabular("Cost/union", mean_union_cost.item())
-
-            logger.log_tabular(
-                "Norm/cost_model",
-                get_tree_norm(nnx.state(cost_model, nnx.Param)),
-            )
-            logger.log_tabular(
-                "Norm/policy_model",
-                get_tree_norm(nnx.state(policy_model, nnx.Param)),
-            )
-            logger.dump_tabular()
-
-        if steps % config["save_freq"] == 0:
-            logger.nn_model_save(
-                itr=steps,
-                nn_model_saver_element=cost_model,
-                prefix="cost",
-            )
-            logger.nn_model_save(
-                itr=steps,
-                nn_model_saver_element=policy_model,
-                prefix="bc_policy",
+            (
+                cost_loss,
+                mean_pos_bag_cost,
+                mean_neg_bag_cost,
+                mean_union_bag_cost,
+                pos_neg_cost_loss,
+                pos_union_cost_loss,
+                union_neg_cost_loss,
+            ) = train_cost_model(
+                cost_model=cost_model,
+                cost_optimizer=cost_optimizer,
+                target_pos_obs=target_pos_obs,
+                target_pos_act=target_pos_act,
+                target_neg_obs=target_neg_obs,
+                target_neg_act=target_neg_act,
+                target_union_obs=target_union_obs,
+                target_union_act=target_union_act,
+                has_positive=has_positive,
+                gamma=config["gamma"],
+                bag_size=config["bag_size"],
             )
 
-        if steps >= config["total_iteration"]:
-            break
+            bc_loss, bc_pos_loss, bc_union_loss = train_policy_model(
+                cost_model=cost_model,
+                bc_policy=bc_policy,
+                bc_optimizer=bc_optimizer,
+                target_pos_obs=target_pos_obs,
+                target_pos_act=target_pos_act,
+                target_union_obs=target_union_obs,
+                target_union_act=target_union_act,
+                has_positive=has_positive,
+                gamma=config["gamma"],
+                temp=config["cost_weight_temp"],
+            )
+
+            logger.logged = False
+
+            if (steps % config["log_freq"] == 0) and (not logger.logged):
+                eval_episodes = config["eval_episode_freq"]
+                if args.use_eval:
+                    eval_start_time = time.time()
+                    for id in range(eval_episodes):
+                        (eval_reward, eval_cost, eval_len) = evaluate_bc_policy(
+                            eval_env, bc_policy.action, mu_obs, std_obs
+                        )
+                        eval_rew_deque.append(eval_reward)
+                        eval_cost_deque.append(eval_cost)
+                        eval_len_deque.append(eval_len)
+                    eval_end_time = time.time()
+
+                    logger.log_tabular("Metrics/EvalEpRet", np.mean(eval_rew_deque))
+                    logger.log_tabular("Metrics/EvalEpCost", np.mean(eval_cost_deque))
+                    logger.log_tabular("Metrics/EvalEpLen", np.mean(eval_len_deque))
+                    logger.log_tabular("Time/Eval", eval_end_time - eval_start_time)
+
+                logger.log_tabular("Train/Steps", steps)
+
+                logger.log_tabular("Loss/Loss_cost", cost_loss.item())
+                logger.log_tabular("Loss/Loss_pos_neg_cost", pos_neg_cost_loss.item())
+                logger.log_tabular(
+                    "Loss/Loss_pos_union_cost", pos_union_cost_loss.item()
+                )
+                logger.log_tabular(
+                    "Loss/Loss_union_neg_cost", union_neg_cost_loss.item()
+                )
+
+                logger.log_tabular("Loss/Loss_bc_policy", bc_loss.item())
+                logger.log_tabular("Loss/Loss_bc_pos_policy", bc_pos_loss.item())
+                logger.log_tabular("Loss/Loss_bc_union_policy", bc_union_loss.item())
+
+                logger.log_tabular("CostPred/pos_bag_cost", mean_pos_bag_cost.item())
+                logger.log_tabular("CostPred/neg_bag_cost", mean_neg_bag_cost.item())
+                logger.log_tabular(
+                    "CostPred/union_bag_cost", mean_union_bag_cost.item()
+                )
+
+                logger.log_tabular(
+                    "Reward/pos", jnp.sum(target_pos_reward, axis=-1).mean().item()
+                )
+                logger.log_tabular(
+                    "Reward/neg", jnp.sum(target_neg_reward, axis=-1).mean().item()
+                )
+                logger.log_tabular(
+                    "Reward/union",
+                    jnp.sum(target_union_reward, axis=-1).mean().item(),
+                )
+
+                logger.log_tabular(
+                    "Cost/pos", jnp.sum(target_pos_cost, axis=-1).mean().item()
+                )
+                logger.log_tabular(
+                    "Cost/neg", jnp.sum(target_neg_cost, axis=-1).mean().item()
+                )
+                logger.log_tabular(
+                    "Cost/union", jnp.sum(target_union_cost, axis=-1).mean().item()
+                )
+
+                logger.log_tabular(
+                    "Norm/cost_model",
+                    get_tree_norm(nnx.state(cost_model, nnx.Param)),
+                )
+                logger.log_tabular(
+                    "Norm/bc_policy",
+                    get_tree_norm(nnx.state(bc_policy, nnx.Param)),
+                )
+                logger.dump_tabular()
+
+            if steps % config["save_freq"] == 0:
+                logger.nn_model_save(
+                    itr=steps,
+                    nn_model_saver_element=cost_model,
+                    prefix="cost",
+                )
+                logger.nn_model_save(
+                    itr=steps,
+                    nn_model_saver_element=bc_policy,
+                    prefix="bc_policy",
+                )
+
+            if steps >= config["total_iteration"]:
+                break
 
     logger.nn_model_save(itr=steps, nn_model_saver_element=cost_model, prefix="cost")
     logger.nn_model_save(
-        itr=steps, nn_model_saver_element=policy_model, prefix="bc_policy"
+        itr=steps, nn_model_saver_element=bc_policy, prefix="bc_policy"
     )
     if config["normalize_observation"]:
         logger.save_state(
