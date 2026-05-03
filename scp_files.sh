7382d93270d9b9971c02c8de7e5a3bc16ec8e01f
returaj <returaj.burnwal@gmail.com>
2026-05-03 15:31:38 +0530

    update scp file

--- scp_files.sh
+++ scp_files.sh
@@ -2,16 +2,16 @@ read -p "pswd: " pswd
 
 hostname=
 
-source_path=/home/cs21d406/github/offline_mpc/dsrl_model/runs/neg_union/safetd3/norm/simultaneous_cost_contrastive/lr_0.00001/batch_128_M_0_hz_5/alpha_0.01_vbeta_1.0_bootlambda_0.0/
-target_path=/mnt/d/books/iitm/phd/offline_mpc/safetd3/neg_50/safetd3/simultaneous/hz_5/lambda_0.0/valpha_0.01_vbeta_1.0/contrastive/
+source_path=/home/cs21d406/github/offline_mpc/dsrl_model/runs/neg_union_full/safecl/pos_1_neg_5_union_500/safecl/plabel_1.0_nlabel_0.0/limit_0.85_embd_250/batch_128_temp_0.1_hz_100/value_nonorm_scale
+target_path=/home/returaj/Documents/research/offline_safe_rl/neg_union_full/pos_1_neg_5_union_500/safecl/value
 
-# for task in 'Circle2' 'Button1' 'Goal1'; do
-for task in 'Ant' 'Swimmer' 'Walker2d'; do
-# for task in 'Ant' 'Swimmer' 'Walker2d' 'Circle2' 'Button1' 'Goal1'; do
+for task in 'PointCircle2' 'CarCircle2' 'PointGoal1' 'CarGoal1'; do
+# for task in 'AntCircle' 'AntRun' 'DroneCircle' 'DroneRun'; do
+# for task in 'AntVelocity' 'SwimmerVelocity' 'Walker2dVelocity' 'HopperVelocity'; do
     echo "Starting $task"
-    for ttype in 'ep_reward' 'ep_cost' 'ep_length' 'ep_worst_cost_0.1' 'ep_worst_cost_0.2' 'ep_worst_cost_0.3' 'ep_worst_cost_0.5'; do
-        
-       sshpass -p $pswd scp -r cs21d406@$hostname:$source_path/*${task}*/*/seed-00*/${ttype}_*  $target_path/*${task}*/${ttype}/
+    for ttype in 'ep_reward' 'ep_cost' 'ep_length' 'ep_worst_cost_0.1' 'ep_worst_cost_0.2' 'ep_worst_cost_0.3' 'ep_worst_cost_0.5' 'dataset'; do
+  
+       sshpass -p $pswd scp -r cs21d406@$hostname:$source_path/*${task}*/*/seed-00*/*${ttype}_*  $target_path/*${task}*/${ttype}/
 
        echo "Done ${ttype} for $task" 
     done
