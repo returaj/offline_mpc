8e44fc0441f95f0542e60af5d7e494f04906c2e3
returaj <returaj.burnwal@gmail.com>
2026-07-05 13:28:32 +0530

    use default value

--- dsrl_model/model_free/safecl_jax.py
+++ dsrl_model/model_free/safecl_jax.py
@@ -43,7 +43,7 @@ from dsrl_model.utils.utils import make_static_config_from_dict, single_agent_ar
 EPS = 1e-6
 
 default_cfg = {
-    "log_freq": int(1e2),
+    "log_freq": int(1e4),
     "save_freq": int(2e4),
     "eval_episode_freq": 1,  # use saved bc_policy to run evaluatation
     "hidden_size": 256,
@@ -61,7 +61,7 @@ default_cfg = {
     "value_th": 0.85,
     "update_tau": 0.01,
     "weight_decay": 0.01,
-    "total_iteration": int(1e3),
+    "total_iteration": int(1e6),
 }
 
 trajectory_cfg = {
