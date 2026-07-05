6a17bd01ac40db0ab91f1d547ab4b9b6b93d3138
returaj <returaj.burnwal@gmail.com>
2026-07-05 13:03:33 +0530

    update notebook and script

--- scp_files.sh
+++ scp_files.sh
@@ -2,11 +2,11 @@ read -p "pswd: " pswd
 
 hostname=andromeda_2
 
-source_path=/home/cs21d406/github/offline_mpc/dsrl_model/runs/neg_union_full/safecl/pos_1_neg_10_union_500/safecl/plabel_1.0_nlabel_-1.0/limit_0.90_embdsize_64_embdfreq_1000/batch_64_vtemp_0.2_mean_std_pitemp_0.2_pialpha_1.0_hz_50/weightdecay_false/norm_piq
-target_path=/home/returaj/Documents/research/offline_safe_rl/neg_union_full/pos_1_neg_10_union_500/safecl/b64_embd64_attnunion/meanstd_piq_temp_0.2_alpha_1.0
+source_path=/home/cs21d406/github/offline_mpc/dsrl_model/runs/neg_union_full/safecl/pos_1_neg_0_union_500/safecl_norandomtraj/plabel_1.0_nlabel_-1.0/limit_0.90_embdsize_64_embdfreq_1000/batch_64_vtemp_1.0_constant_pitemp_0.2_pialpha_0.9_hz_50/weightdecay_true/norm_piq_starttraj_removeunionlmbda_constant_projection/
+target_path=/home/returaj/Documents/research/offline_safe_rl/neg_union_full/pos_1_neg_0_union_500/safecl_il_noreg
 
-for task in 'AntVelocity' 'PointCircle2' 'AntRun' 'DroneRun'; do # 'SwimmerVelocity' 'CarCircle2' 'PointGoal1' 'CarGoal1' 'AntCircle' 'DroneCircle'; do
-# for task in 'AntCircle' 'AntRun' 'DroneCircle' 'DroneRun'; do
+for task in 'AntVelocity' 'PointCircle2' 'AntRun' 'DroneRun' 'SwimmerVelocity' 'CarCircle2' 'AntCircle' 'DroneCircle' 'PointGoal1' 'CarGoal1'; do
+# for task in 'AntCircle' 'DroneCircle' 'CarCircle2'; do
 # for task in 'AntVelocity' 'SwimmerVelocity' 'Walker2dVelocity' 'HopperVelocity'; do
     echo "Starting $task"
     for ttype in 'ep_reward' 'ep_cost' 'ep_length' 'ep_worst_cost_0.1' 'ep_worst_cost_0.2' 'ep_worst_cost_0.3' 'ep_worst_cost_0.5' 'dataset'; do
